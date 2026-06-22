4106459e7b422064d45f8fbe4c6fd639d7cf95b8
bill-moj <167539377+bill-moj@users.noreply.github.com>
2026-06-22 09:39:16 +0100

    Setup sentry (#4)

--- server/config.ts
+++ server/config.ts
@@ -127,4 +127,12 @@ export default {
   ingressUrl: get('INGRESS_URL', 'http://localhost:3000', requiredInProduction),
   environmentName: get('ENVIRONMENT_NAME', ''),
   appInsightsConnectionString: get('APPLICATIONINSIGHTS_CONNECTION_STRING', '', requiredInProduction),
+  sentry: {
+    dsn: process.env['SENTRY_DSN'],
+    loaderScriptId: process.env['SENTRY_LOADER_SCRIPT_ID'],
+    environment: get('SENTRY_ENVIRONMENT', 'local', requiredInProduction),
+    tracesSampleRate: Number(get('SENTRY_TRACES_SAMPLE_RATE', 0.05)),
+    replaySampleRate: Number(get('SENTRY_REPLAY_SAMPLE_RATE', 0.0)),
+    replayOnErrorSampleRate: Number(get('SENTRY_REPLAY_ON_ERROR_SAMPLE_RATE', 0.1)),
+  },
 }
---
+++ server/sentry.ts
@@ -0,0 +1,11 @@
+import * as Sentry from '@sentry/node'
+import config from './config'
+
+if (config.sentry.dsn) {
+  Sentry.init({
+    dsn: config.sentry.dsn,
+    environment: config.sentry.environment,
+    tracesSampleRate: config.sentry.tracesSampleRate,
+    integrations: [Sentry.httpIntegration({ spans: false })],
+  })
+}
