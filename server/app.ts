b791897842efa38b9c30873d9a04bddf62e3c27f
bill-moj <167539377+bill-moj@users.noreply.github.com>
2026-09-14 11:41:14 +0100

    Check service enabled (#52)
    * Add permission check for service enablement

--- server/app.ts
+++ server/app.ts
@@ -27,6 +27,7 @@ import { handleApiError } from './middleware/validation/handleApiError'
 import sentryMiddleware from './middleware/sentryMiddleware'
 import { AuthorisedRoles } from './middleware/permissions/populateUserPermissions'
 import addUsernameAndCaseloadToTelemetry from './utils/azureAppInsights'
+import { serviceEnabledMiddleware } from './middleware/permissions/serviceEnabledMiddleware'
 
 export default function createApp(services: Services): express.Application {
   const app = express()
@@ -92,6 +93,8 @@ export default function createApp(services: Services): express.Application {
   )
 
   app.use(addUsernameAndCaseloadToTelemetry())
+
+  app.get(/(.*)/, serviceEnabledMiddleware)
 
   app.use(routes(services))
 
